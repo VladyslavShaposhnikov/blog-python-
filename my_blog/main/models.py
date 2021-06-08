f710fb08d738ffb125d895990842a9a6935f7791
VladyslavShaposhnikov <shapik2404@gmail.com>
2021-06-08 14:58:09 +0200

    created urls, template, superuser; home view, model post

--- my_blog/main/models.py
+++ my_blog/main/models.py
@@ -1,3 +1,21 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class Post(models.Model):
+
+    STATUS = {
+        (0,'Draft'),
+        (1,'Publish')
+    }
+
+    title = models.CharField(max_length=255)
+    body = models.TextField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    updated_on = models.DateTimeField(auto_now=True)
+    published_on = models.DateTimeField(auto_now_add=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    status = models.IntegerField(choices=STATUS, default=0)
+
+    def __str__(self):
+        return self.title + '|' + self.author
+
