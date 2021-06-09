1e4126c47d1c418ffa8398bbe9f3a659a715b5ea
VladyslavShaposhnikov <shapik2404@gmail.com>
2021-06-09 16:32:43 +0200

    created add_post.html; in views: add field 'order by' , in models: add 'get_absolute_url'

--- my_blog/main/urls.py
+++ my_blog/main/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import HomeView, ArticleDetailView
+from .views import HomeView, ArticleDetailView, AddPostView
 
 urlpatterns = [
     path('', HomeView.as_view(), name='home'),
-    path('<slug:slug>/', ArticleDetailView.as_view(), name='article_detaile'),
+    path('add-post/', AddPostView.as_view(), name='add-post'),
+    path('<slug:slug>/', ArticleDetailView.as_view(), name='article_detail'),
 ]
--- my_blog/main/views.py
+++ my_blog/main/views.py
@@ -1,11 +1,17 @@
 from django.shortcuts import render
-from django.views.generic import ListView, DetailView
-from . models import Post
+from django.views.generic import ListView, DetailView, CreateView
+from .models import Post
 
 class HomeView(ListView):
     model = Post
     template_name = 'home.html'
+    queryset = Post.objects.order_by('-published_on')
 
 class ArticleDetailView(DetailView):
     model = Post
     template_name = 'article_detail.html'
+
+class AddPostView(CreateView):
+    model = Post
+    template_name = 'add_post.html'
+    fields = 'title', 'body', 'author', 'slug'
